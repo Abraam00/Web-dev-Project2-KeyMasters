4c63cdef071897c49ed8344771586127fbf41e6d
Abraam00 <89326446+Abraam00@users.noreply.github.com>
2023-03-29 12:25:24 -0400

    front end template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,26 @@
+import "./bootstrap.min.css";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [teamName, setTeamName] = useState("");
+
+  const handleInputChange = (event) => {
+    setTeamName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Team name entered: ${teamName}`);
+  };
   return (
-    <div className="App">
-      <header></header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter team name:
+        <input type="text" value={teamName} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Enter</button>
+    </form>
   );
 }
 
