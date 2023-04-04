d6c5d8bf33869946485176a6a7b48a2152a75d9f
Rytsumusic <89271300+Rytsumusic@users.noreply.github.com>
2023-04-04 12:14:42 -0400

    added groundwork for both the leadboard and user/admin controller

---
+++ node-authentication-notes/controllers/User.controller.js
@@ -0,0 +1,122 @@
+const User = require('../models/note.model.js');
+
+// Create and Save a new User code id
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Create a Note
+    const user = new User({
+        title: req.body.title || "Untitled team name",
+        content: req.body.content,
+    });
+
+    // Save Note in the database
+    user.save()
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the qr id."
+            });
+        });
+};
+
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+    User.find()
+        .then(user => {
+            res.send(user);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving notes."
+            });
+        });
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+    User.findById(req.params.noteId)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            if (user.email == req.user) {
+                res.send(note);
+            }
+            else {
+                res.send({});
+            }
+
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving note with id " + req.params.noteId
+            });
+        });
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Find note and update it with the request body
+    User.findByIdAndUpdate(req.params.noteId, {
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    }, { new: true })
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            res.send(user);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            return res.status(500).send({
+                message: "Error updating note with id " + req.params.noteId
+            });
+        });
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+    User.findByIdAndRemove(req.params.noteId)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({
+                    message: "User not found with id " + req.params.noteId
+                });
+            }
+            res.send({ message: "Note deleted successfully!" });
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            return res.status(500).send({
+                message: "Could not delete note with id " + req.params.noteId
+            });
+        });
+};
