685687e25494c67eea7c2953138caf203546f16f
jxgonza <61395823+jxgonza@users.noreply.github.com>
2023-04-10 12:32:15 -0400

    Changes to Navbar and Admin

--- my-app/src/Admin.js
+++ my-app/src/Admin.js
@@ -1,6 +1,7 @@
 import "./bootstrap.min.css";
 import "./App.css";
 import React, { useState } from "react";
+import QrCode from "qrcode.react";
 
 function Admin() {
   const [hint, setHint] = useState("");
@@ -12,40 +13,11 @@ function Admin() {
   };
 
   return (
-    <div className="admin-page">
-      <div className="admin-banner">
+    <div>
+      <div className="adminTopBanner">
         <h1>Admin Page</h1>
       </div>
-      <div className="admin-body">
-        <div className="create-qr">
-          {!showQR ? (
-            <button
-              className="create-qr-button"
-              onClick={() => setShowQR(true)}
-            >
-              Create QR
-            </button>
-          ) : (
-            <div className="qr-code-container">
-              <QrCode value={hint} size={200} />
-              <button className="create-qr-button">Create QR</button>
-            </div>
-          )}
-          {showQR && (
-            <form className="hint-form" onSubmit={handleHintSubmit}>
-              <label htmlFor="hint-input">Hint or Clue:</label>
-              <input
-                id="hint-input"
-                type="text"
-                value={hint}
-                onChange={(e) => setHint(e.target.value)}
-                required
-              />
-              <button type="submit">Submit</button>
-            </form>
-          )}
-        </div>
-      </div>
+      <div className="adminFormGroup">Test</div>
     </div>
   );
 }
