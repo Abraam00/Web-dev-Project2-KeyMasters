178ee6846c118d54b2bccc756242a77048b9f1fb
Rytsumusic <89271300+Rytsumusic@users.noreply.github.com>
2023-04-19 11:47:03 -0400

    Update leaderboardFunctions.js

--- my-app/src/functions/leaderboardFunctions.js
+++ my-app/src/functions/leaderboardFunctions.js
@@ -2,30 +2,34 @@ import axios from "axios";
 const config = require("../config");
 
 export async function showLeaderboard() {
-  return axios.get(`${config.API.BASE_URL}/leaderboard/top10`, {});
+  return axios.get(`${config.API.BASE_URL}/leaderboard/top10`);
 }
 
 export async function createTeam(team) {
   return await axios
-    .post(`${config.API.BASE_URL}/leaderboard/create`, {
-      teamName: team,
+    .post(`${config.API.BASE_URL}/leaderboard/create`, 
+      {teamName: team},
+      {
       headers: {
         "Content-Type": "application/json",
-      },
-    })
+        },
+      } 
+    )
     .then((response) => console.log(response.data))
     .catch((err) => console.log(err));
 }
 
 export async function updateTeam(team, url) {
   return await axios
-    .post(`${config.API.BASE_URL}/leaderboard/update`, {
-      teamName: team,
-      _found: url,
+    .post(`${config.API.BASE_URL}/leaderboard/update`,
+      {teamName: team,
+      _found: url},
+      {
       headers: {
         "Content-Type": "application/json",
       },
-    })
+    }
+    )
     .then((response) => console.log(response.data))
     .catch((err) => console.log(err));
 }
